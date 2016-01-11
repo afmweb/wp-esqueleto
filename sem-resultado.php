220f323a7a44797dfcdd76c705c4ca6ddf083b90
AndreMarcelino <marcelinoandre@gmail.com>
2016-01-11 18:50:54 -0200

    refatorando a estrutura do tema

--- sem-resultado.php
+++
@@ -1,5 +0,0 @@
-<h2>Ops! Não encontramos esse conteúdo.</h2>
-
-<p>Aqui poderíamos incluir um arquivo de um formulário de pesquisa ...</p>
-
-<p>Também poderíamos listar os últimos posts do blog ...</p>
